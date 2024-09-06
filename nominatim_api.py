832d1dbaa7121da9a6a42db05e05036ca4b5da22
djohoe28 <djohoe28@gmail.com>
2024-09-06 18:19:01 +0300

    crypter.py documentation linting.

--- nominatim_api.py
+++ nominatim_api.py
@@ -10,12 +10,12 @@ class NominatimAPI:
     """
     An interface for making requests to the Nominatim API.
 
-    :var str API_URL: The URL to use when sending a request to the external API.
-    :var str API_ENDPOINT: The endpoint to use when sending a request to the external API.
-    :var str API_PARAMETER_NAME: Parameter Name to use when sending a request to the external API.
-    :var dict API_PARAMS: Parameters to use when sending a request to the external API.
-    :var dict API_HEADERS: Headers to use when sending a request to the external API.
-    :var int API_RATE: API rate limit in requests per second.
+    :var API_URL: The URL to use when sending a request to the API.
+    :var API_ENDPOINT: The endpoint to use when sending a request to the API.
+    :var API_PARAMETER_NAME: Parameter Name to use when sending a request to the API.
+    :var API_PARAMS: Parameters to use when sending a request to the API.
+    :var API_HEADERS: Headers to use when sending a request to the API.
+    :var API_RATE: API rate limit in requests per second.
     """
     API_URL = "https://nominatim.openstreetmap.org"
     API_ENDPOINT = "search"
@@ -32,7 +32,12 @@ class NominatimAPI:
 
     @classmethod
     def query_location(cls, query: str):
-        """Returns the first matching `Location` for the given `query`."""
+        """
+        Returns the first matching :class:`Location` for the given :param:`query`.
+
+        :param query: Query describing the desired location.
+        :return: The first matching location received from Nominatim.
+        """
         time.sleep(cls.API_RATE)
         response = requests.get(urljoin(cls.API_URL, cls.API_ENDPOINT),
                                 params={**cls.API_PARAMS, cls.API_PARAMETER_NAME: query},
